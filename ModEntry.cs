cb9a4acb73867c8ac3d44ea1f21b7b60ef609433
bwdy <47769190+bwdy@users.noreply.github.com>
2022-03-21 20:26:42 -0800

    Project setup

---
+++ ModEntry.cs
@@ -0,0 +1,13 @@
+﻿using StardewModdingAPI;
+using System;
+
+namespace SDVFactory
+{
+    public class ModEntry : StardewModdingAPI.Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            Monitor.Log("Hello world from Factory!", LogLevel.Alert);
+        }
+    }
+}
