7bf4a1298c69425592d07e5d2f9ed60e062da6e7
Алмаз <almgal353@gmail.com>
2022-09-07 15:32:02 +0500

    Code cleanup.

--- MinecartPatcher/MCPModHooks.cs
+++ MinecartPatcher/MCPModHooks.cs
@@ -11,66 +11,66 @@ namespace MinecartPatcher
 {
     public class MCPModHooks : ModHooks
     {
-		ModHooks alias;
-		ModEntry modEntry;
+        ModHooks alias;
+        ModEntry modEntry;
 
-		public MCPModHooks(ModEntry mod)
+        public MCPModHooks(ModEntry mod)
         {
-			modEntry = mod;
-			var field = typeof(Game1).GetField("hooks", BindingFlags.Static | BindingFlags.NonPublic);
-			alias = (ModHooks) field.GetValue(null);
-			field.SetValue(null, this);
-		}
+            modEntry = mod;
+            var field = typeof(Game1).GetField("hooks", BindingFlags.Static | BindingFlags.NonPublic);
+            alias = (ModHooks)field.GetValue(null);
+            field.SetValue(null, this);
+        }
 
-		public override void OnGame1_PerformTenMinuteClockUpdate(Action action)
-		{
-			alias.OnGame1_PerformTenMinuteClockUpdate(action);
-		}
+        public override void OnGame1_PerformTenMinuteClockUpdate(Action action)
+        {
+            alias.OnGame1_PerformTenMinuteClockUpdate(action);
+        }
 
-		public override void OnGame1_NewDayAfterFade(Action action)
-		{
-			alias.OnGame1_NewDayAfterFade(action);
-		}
+        public override void OnGame1_NewDayAfterFade(Action action)
+        {
+            alias.OnGame1_NewDayAfterFade(action);
+        }
 
-		public override void OnGame1_ShowEndOfNightStuff(Action action)
-		{
-			alias.OnGame1_ShowEndOfNightStuff(action);
-		}
+        public override void OnGame1_ShowEndOfNightStuff(Action action)
+        {
+            alias.OnGame1_ShowEndOfNightStuff(action);
+        }
 
-		public override void OnGame1_UpdateControlInput(ref KeyboardState keyboardState, ref MouseState mouseState, ref GamePadState gamePadState, Action action)
-		{
-			alias.OnGame1_UpdateControlInput(ref keyboardState, ref mouseState, ref gamePadState, action);
-		}
+        public override void OnGame1_UpdateControlInput(ref KeyboardState keyboardState, ref MouseState mouseState, ref GamePadState gamePadState, Action action)
+        {
+            alias.OnGame1_UpdateControlInput(ref keyboardState, ref mouseState, ref gamePadState, action);
+        }
 
-		public override void OnGameLocation_ResetForPlayerEntry(GameLocation location, Action action)
-		{
-			alias.OnGameLocation_ResetForPlayerEntry(location, action);
-		}
+        public override void OnGameLocation_ResetForPlayerEntry(GameLocation location, Action action)
+        {
+            alias.OnGameLocation_ResetForPlayerEntry(location, action);
+        }
 
-		public override bool OnGameLocation_CheckAction(GameLocation location, Location tileLocation, xTile.Dimensions.Rectangle viewport, Farmer who, Func<bool> action)
-		{
-			var mc = modEntry.FindMinecart(location, new Vector2(tileLocation.X, tileLocation.Y));
-			if (mc != null)
+        public override bool OnGameLocation_CheckAction(GameLocation location, Location tileLocation, xTile.Dimensions.Rectangle viewport, Farmer who, Func<bool> action)
+        {
+            var mc = modEntry.FindMinecart(location, new Vector2(tileLocation.X, tileLocation.Y));
+            if (mc != null)
             {
-				modEntry.OnMinecartActivation(mc, location, new Vector2(tileLocation.X, tileLocation.Y));
-				return true;
+                modEntry.OnMinecartActivation(mc, location, new Vector2(tileLocation.X, tileLocation.Y));
+                return true;
             }
-			return alias.OnGameLocation_CheckAction(location, tileLocation, viewport, who, action);
-		}
+            return alias.OnGameLocation_CheckAction(location, tileLocation, viewport, who, action);
+        }
 
-		public override FarmEvent OnUtility_PickFarmEvent(Func<FarmEvent> action)
-		{
-			return alias.OnUtility_PickFarmEvent(action);
-		}
+        public override FarmEvent OnUtility_PickFarmEvent(Func<FarmEvent> action)
+        {
+            return alias.OnUtility_PickFarmEvent(action);
+        }
 
-		public override Task StartTask(Task task, string id)
-		{ 
-			return alias.StartTask(task, id);
-		}
+        public override Task StartTask(Task task, string id)
+        {
+            return alias.StartTask(task, id);
+        }
 
-		public override Task<T> StartTask<T>(Task<T> task, string id)
-		{
-			return alias.StartTask<T>(task, id);
-		}
-	}
+        public override Task<T> StartTask<T>(Task<T> task, string id)
+        {
+            return alias.StartTask<T>(task, id);
+        }
+    }
 }
